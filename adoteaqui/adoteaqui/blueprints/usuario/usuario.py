e0e6efa3d31b5840a9f386744a6e7dd2383b7499
Eduardo Arruda <eduardoarrudag@gmail.com>
2021-10-11 17:08:02 +0000

    adicionei o bootstrap 5 e as minhas páginas de cadastro e login

--- adoteaqui/adoteaqui/blueprints/usuario/usuario.py
+++ adoteaqui/adoteaqui/blueprints/usuario/usuario.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='templates')
@@ -8,6 +8,14 @@ bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='temp
 def root():
     return 'Hello from usuario'
 
+@bp.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
 
 def init_app(app):
     app.register_blueprint(bp)
