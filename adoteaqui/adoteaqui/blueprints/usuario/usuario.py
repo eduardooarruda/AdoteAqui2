ca9ad65b13452ee36e131e5b881a25a228cec25c
Eduardo Arruda <eduardoarrudag@gmail.com>
2021-10-14 19:35:25 +0000

    Adicionei todos os Estados brasileiros no formulário de cadastro

--- adoteaqui/adoteaqui/blueprints/usuario/usuario.py
+++ adoteaqui/adoteaqui/blueprints/usuario/usuario.py
@@ -6,13 +6,41 @@ from flask import Blueprint, render_template
 bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='templates')
 
 
+Estados ={
+    'Acre':'AC',
+    'Alagoas': 'AL',
+    'Amapá': 'AP',
+    'Amazonas': 'AM',
+    'Bahia': 'BA',
+    'Ceará': 'CE',
+    'Distrito Federal': 'DF',
+    'Espírito Santo': 'ES',
+    'Goiás': 'GO',
+    'Maranhão': 'MA',
+    'Mato Grosso do Sul':'MS',
+    'Minas Gerais': 'MG',
+    'Pará': 'PA',
+    'Paraíba': 'PB',
+    'Paraná': 'PR',
+    'Pernambuco': 'PE',
+    'Piauí': 'PI',
+    'Rio de Janeiro': 'RJ',
+    'Rio Grande do Norte': 'RN',
+    'Rio Grande do Sul': 'RS',
+    'Rondônia': 'RO',
+    'Roraima': 'RR',
+    'Santa Catarina': 'SC',
+    'São Paulo': 'SP',
+    'Sergipe': 'SE',
+    'Tocantins': 'TO'
+}
 # @bp.route('/')
 # def root():
 #     return 'Hello from usuario'
 
 @bp.route('/cadastro')
 def cadastro():
-    return render_template('cadastro.html')
+    return render_template('cadastro.html', Estados=Estados)
 
 
 @bp.route('/login')
