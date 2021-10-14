5a3a68e0c0a5ba180a4c2abd597e3a2edd744858
Eduardo Arruda <eduardoarrudag@gmail.com>
2021-10-14 00:47:55 +0000

    Coloquei as rotas de cadastro no Blueprint autenticacao, também fiz o uso do url_for

--- adoteaqui/adoteaqui/blueprints/autenticacao/autenticacao.py
+++ adoteaqui/adoteaqui/blueprints/autenticacao/autenticacao.py
@@ -1,13 +1,28 @@
-from flask import Blueprint
-
+from flask import Blueprint, redirect, url_for, request, flash
+from ...ext.database import db
+from ..usuario.entidades import Usuario
 
 bp = Blueprint('autenticacao', __name__, url_prefix='/autenticacao')
 
 
-@bp.route('/')
-def root():
-    return 'Hello from autenticacao'
+# @bp.route('/')
+# def root():
+#     return 'Hello from autenticacao'
 
+@bp.post('/cadastrarUsuario')
+def cadastrarUsuario():
+    usuario = Usuario()
+    usuario.username = request.form['username']
+    usuario.nome = request.form['nome']
+    usuario.senha = request.form['senha']
+    usuario.telefone = request.form['telefone']
+    usuario.email = request.form['email']
+
+    db.session.add(usuario)
+    db.session.commit()
+
+    flash('Cadastro feito com sucesso!')
+    return redirect(url_for('usuario.login'))
 
 def init_app(app):
     app.register_blueprint(bp)
--- adoteaqui/adoteaqui/blueprints/usuario/usuario.py
+++ adoteaqui/adoteaqui/blueprints/usuario/usuario.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, flash, url_for
-from .entidades import Usuario
-from ...ext.database import db
+from flask import Blueprint, render_template
+# from .entidades import Usuario
+# from ...ext.database import db
 
 
 bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='templates')
@@ -13,21 +13,6 @@ bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='temp
 @bp.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
-
-@bp.post('/cadastrarUsuario')
-def cadastrarUsuario():
-    usuario = Usuario()
-    usuario.username = request.form['username']
-    usuario.nome = request.form['nome']
-    usuario.senha = request.form['senha']
-    usuario.telefone = request.form['telefone']
-    usuario.email = request.form['email']
-
-    db.session.add(usuario)
-    db.session.commit()
-
-    flash('Cadastro feito com sucesso!')
-    return redirect(url_for('usuario.login'))
 
 
 @bp.route('/login')
